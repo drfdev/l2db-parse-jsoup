1635e29405a344b3d6a90902f3d6453534bd5c4e
drfdev <drafff@gmail.com>
2020-02-09 23:18:13 +0300

    Working!

--- src/main/java/dev/drf/l2db/Main.java
+++ src/main/java/dev/drf/l2db/Main.java
@@ -4,39 +4,75 @@ import dev.drf.l2db.conf.Resource;
 import dev.drf.l2db.data.ResultData;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.function.Predicate;
 
 public class Main {
+    private static final int ATTEMPTS = 3;
+    private static final int TIME_TO_SLEEP = 500;
+
     public static void main(String[] args) throws IOException {
         System.out.println("start");
 
         final int size = Resource.values().length;
+//        final int cores = Runtime.getRuntime().availableProcessors();
+
         Map<Resource, ResultData> parseData = new ConcurrentHashMap<>(size);
-        Executor pool = Executors.newFixedThreadPool(size);
+//        ExecutorService pool = Executors.newFixedThreadPool(cores);
 
         for (Resource r : Resource.values()) {
-            pool.execute(() -> {
+//            pool.execute(() -> {
                 String url = r.getUrl();
-                ResultData resultData = new ResultData();
-                try {
-                    Document doc = Jsoup.connect(url).get();
 
-                    // TODO
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                ResultData resultData = parseUrl(url);
+
                 parseData.put(r, resultData);
-            });
+//            });
+        }
+
+//        pool.shutdown();
+
+        long failedCount = parseData.values().stream()
+                .filter(ResultData::isFailed)
+                .count();
+        if (failedCount > 0) {
+            System.out.println("Failed: " + failedCount);
+        } else {
+            System.out.println("All done");
         }
 
         System.out.println("end");
+    }
+
+    private static ResultData parseUrl(String url) {
+        ResultData resultData = new ResultData();
+        int attempt = ATTEMPTS;
+        boolean done = false;
+
+        while (!done || attempt > 0) {
+            try {
+                Document doc = Jsoup.connect(url).get();
+
+                // TODO
+                done = true;
+                resultData.notFailed();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            attempt --;
+            if (!done) {
+                try {
+                    Thread.sleep(TIME_TO_SLEEP);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    attempt = -1;
+                }
+            }
+        }
+        return resultData;
     }
 }
--- src/main/java/dev/drf/l2db/data/ResultData.java
+++ src/main/java/dev/drf/l2db/data/ResultData.java
@@ -4,9 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class ResultData {
+    private boolean failed = true;
     private List<MobInfo> mobs = new ArrayList<>();
 
     public void addMob(MobInfo mobInfo) {
         mobs.add(mobInfo);
+    }
+
+    public void notFailed() {
+        this.failed = false;
+    }
+
+    public boolean isFailed() {
+        return failed;
+    }
+
+    public void setFailed(boolean failed) {
+        this.failed = failed;
+    }
+
+    public List<MobInfo> getMobs() {
+        return mobs;
+    }
+
+    public void setMobs(List<MobInfo> mobs) {
+        this.mobs = mobs;
     }
 }
